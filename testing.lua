d1d314891137c0ca8df34deac91a19779123cf06
Florian Polster <fpolster@uni-koblenz.de>
2015-05-08 17:27:54 +0200

    labeldatei laden

--- testing.lua
+++ testing.lua
@@ -3,25 +3,36 @@ require 'image'
 require 'nn'     
 require 'nnx'      -- provides a normalization operator
 require 'lfs'
+require 'json'
+
+cmd = torch.CmdLine()
+file = "../images/Small/artificial light - 120cm - 20 degrees - 70cm/cocktailglass1_phatch.png"      
+
+cmd:option('-save', file, 'subdirectory to save/log experiments in')
+cmd:text()
+opt = cmd:parse(arg or {})
+file = opt.save
 
 model = torch.load('results/model.net')
-classes = {'cocktailglass','colaglass','shot','waterglass','wineglass','whitebeerglass', 'meh' } --TODO NEED TO SAVE AND LOAD THAT ONE
+local labelsFile = '../images/tensors/labels.txt'
+f = io.open(labelsFile) 
+classes = json.decode(f:read("*all")) 
 
 model:evaluate()
 
 -- test over test data
-print'==> testing'
-print (classes)
+--print'==> testing'
+--print (classes)
 
 
-file = "../images/Small/colaglass1.png"      
+
 testimage = torch.Tensor(1, 3, 32, 32)
 image.rgb2yuv(testimage,image.load(file))
 
 -- test sample
 result = model:forward(testimage)
 
-print(result)
+--print(result)
 
 local key = 1
 local max = result[key]
@@ -33,9 +44,9 @@ for k = 1,  result:size(1) do
     end
 end
 
-print(key, max)
+--print(key, max)
 
-print(sys.COLORS.red .. classes[key])
+print(classes[key])
 
 
 
