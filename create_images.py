f12c796027dd73cc0664b9d3e05da3ca019e11c2
devilmore <abarthen@uni-koblenz.de>
2015-05-12 18:27:24 +0200

    Added first version of image creation script which takes a video, crops it and saves random frames. Still needs image detection for final images.

---
+++ create_images.py
@@ -0,0 +1,19 @@
+from random import randint
+from moviepy.editor import *
+from PIL import *
+
+# Open video and determine total number of frames
+video = VideoFileClip("../videos/Input.ogv")
+frames = video.fps * video.duration
+
+for x in range(0, 100):
+	# Choose a random frame
+	i = randint(0,frames)
+	# Save the image (it would be better if this step was unnecessary but moviepy is being a bitch about it)
+	output_str = '../videos/output' + str(i) + '.jpg'
+	video.save_frame(output_str, t=i)
+	# Open the image again, choose the needed area (this has to be changed or ommitted for different setups) and save it again, overwriting the first file
+	image = Image.open(output_str)
+	box = (20,40,490,510)
+	region = image.crop(box)
+	region.save(output_str)
